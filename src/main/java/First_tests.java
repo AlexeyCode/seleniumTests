57858de40236f301ca0ff8e195c0c1c7fcc7fd60
ayudin <erasecorp@hotmail.com>
2018-12-28 15:13:56 +0300

    refactor

--- src/main/java/First_tests.java
+++ src/main/java/First_tests.java
@@ -10,6 +10,7 @@ import java.util.concurrent.TimeUnit;
 public class First_tests {
     public static void main( String[] args ) {
         WebDriver driver = new ChromeDriver();
+        driver.manage().deleteAllCookies();
         driver.get("https://en.wikipedia.org");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
@@ -29,17 +30,15 @@ public class First_tests {
 
         driver.findElement(By.xpath("//span[text()=\"Palladium\"]")).click();
         driver.findElement(By.xpath("//button[@class=\"uls-settings-trigger\" and @title=\"Language settings\"]")).click();
-//        try{
-//            Thread.sleep(3);
-//        }catch (Exception e) {
-//            System.out.println(e.getMessage());
-//
-//        }
-        new WebDriverWait(driver,15).until(ExpectedConditions.presenceOfElementLocated (By.xpath("//button[text()=\"Fonts\"]"))).click();
-        new WebDriverWait(driver,15).until(ExpectedConditions.presenceOfElementLocated (By.xpath("//label[@class=\"checkbox\" and @for=\"webfonts-enable-checkbox\"]"))).click();
-        if(driver.findElement(By.xpath("//input[@type=\"checkbox\" and @id=\"webfonts-enable-checkbox\"]")).isSelected()){
-            System.out.println("Selected");
+        try{
+            Thread.sleep(10000);
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+
         }
+        new WebDriverWait(driver,15).until(ExpectedConditions.presenceOfElementLocated (By.xpath("//button[text()=\"Fonts\"]"))).click();
+        new WebDriverWait(driver,15).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class=\"mw-ui-checkbox\"]/input"))).click();
+
         driver.findElement(By.xpath("//button[text()=\"Apply settings\"]")).click();
 
 

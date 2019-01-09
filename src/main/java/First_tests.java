e47e2490a732337a3bcb4e77aa847f5f17366d59
ayudin <erasecorp@hotmail.com>
2019-01-09 13:20:24 +0300

    refactor

--- src/main/java/First_tests.java
+++ src/main/java/First_tests.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -14,7 +15,7 @@ public class First_tests {
         driver.get("https://en.wikipedia.org");
         driver.manage().window().maximize();
 
-        new WebDriverWait(driver,10).until(ExpectedConditions.visibilityOfElementLocated(
+        new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOfElementLocated(
                 By.xpath("//input[@id=\"searchInput\"]"))).sendKeys("Selenium");
 
         driver.findElement(By.xpath("//input[@id=\"searchButton\"]")).click();
@@ -28,32 +29,42 @@ public class First_tests {
         new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(
                 By.xpath("//span[@class=\"toctext\" and text()=\"Examples\"]"))).click();
 
-        new WebDriverWait(driver,10).until(ExpectedConditions.elementToBeClickable(
+        new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(
                 By.xpath("//a[@title=\"Niobium diselenide\"]"))).click();
 
-        new WebDriverWait(driver,10).until(ExpectedConditions.elementToBeClickable(
+        new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(
                 By.xpath("//a[@title=\"Niobium\" and text()=\"Niobium\"]"))).click();
 
-        new WebDriverWait(driver,10).until(ExpectedConditions.elementToBeClickable(
+        new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(
                 By.xpath("//span[text()=\"Palladium\"]"))).click();
 
         new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(
                 By.xpath("//button[@class=\"uls-settings-trigger\" and @title=\"Language settings\"]"))).click();
+//        try{
+//            Thread.sleep(1000);
+//        }catch (Exception e) {
+//            System.out.println(e.getMessage());
+//
+//        }
+        Actions action = new Actions(driver);
+        WebElement fontButton = new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//button[text()=\"Fonts\"]")));
 
-        new WebDriverWait(driver,15).until(ExpectedConditions.elementToBeClickable(
-                By.xpath("//button[text()=\"Fonts\"]"))).click();
-        new WebDriverWait(driver,15).until(ExpectedConditions.presenceOfElementLocated(
+        action.doubleClick(fontButton).perform();
+
+
+        new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(
                 By.xpath("//div[@class=\"mw-ui-checkbox\"]"))).click();
-        try{
-            Thread.sleep(1000);
-        }catch (Exception e) {
-            System.out.println(e.getMessage());
 
-        }
-        new WebDriverWait(driver,15).until(ExpectedConditions.presenceOfElementLocated(
+        new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//select[@id=\"content-font-selector\"]"))).click();
+
+        new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//option[@value=\"ComicNeue\"]"))).click();
+
+        new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(
                 By.xpath("//button[text()=\"Apply settings\"]"))).click();
-
-
+        driver.quit();
 
     }
 }

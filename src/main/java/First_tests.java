68998020843dcd9c9f43d320bec4f5659dfc026f
ayudin <erasecorp@hotmail.com>
2019-01-09 12:15:06 +0300

    refactor

--- src/main/java/First_tests.java
+++ src/main/java/First_tests.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.concurrent.TimeUnit;
+
 
 public class First_tests {
     public static void main( String[] args ) {
@@ -13,33 +13,45 @@ public class First_tests {
         driver.manage().deleteAllCookies();
         driver.get("https://en.wikipedia.org");
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-        WebElement search = driver.findElement(By.xpath("//input[@id=\"searchInput\"]"));
-        search.sendKeys("Selenium");
+        new WebDriverWait(driver,10).until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//input[@id=\"searchInput\"]"))).sendKeys("Selenium");
+
         driver.findElement(By.xpath("//input[@id=\"searchButton\"]")).click();
-        driver.findElement(By.xpath("//a[text()=\"Contents\"]")).click();
-        WebElement search2 = driver.findElement(By.xpath("//input[@id=\"searchInput\"]"));
-        search2.sendKeys("Selenide");
-        search2.submit();
-        driver.findElement(By.xpath("//span[@class=\"toctext\" and text()=\"Examples\"]")).click();
+        new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//a[text()=\"Contents\"]"))).click();
+        WebElement wait = new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//input[@id=\"searchInput\"]")));
+        wait.sendKeys("Selenide");
+        wait.submit();
 
-        driver.findElement(By.xpath("//a[@title=\"Niobium diselenide\"]")).click();
-        driver.findElement(By.xpath("//a[@title=\"Niobium\" and text()=\"Niobium\"]")).click();
+        new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//span[@class=\"toctext\" and text()=\"Examples\"]"))).click();
 
+        new WebDriverWait(driver,10).until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//a[@title=\"Niobium diselenide\"]"))).click();
 
-        driver.findElement(By.xpath("//span[text()=\"Palladium\"]")).click();
-        driver.findElement(By.xpath("//button[@class=\"uls-settings-trigger\" and @title=\"Language settings\"]")).click();
+        new WebDriverWait(driver,10).until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//a[@title=\"Niobium\" and text()=\"Niobium\"]"))).click();
+
+        new WebDriverWait(driver,10).until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//span[text()=\"Palladium\"]"))).click();
+
+        new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//button[@class=\"uls-settings-trigger\" and @title=\"Language settings\"]"))).click();
+
+        new WebDriverWait(driver,15).until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//button[text()=\"Fonts\"]"))).click();
+        new WebDriverWait(driver,15).until(ExpectedConditions.presenceOfElementLocated(
+                By.xpath("//div[@class=\"mw-ui-checkbox\"]"))).click();
         try{
-            Thread.sleep(10000);
+            Thread.sleep(1000);
         }catch (Exception e) {
             System.out.println(e.getMessage());
 
         }
-        new WebDriverWait(driver,15).until(ExpectedConditions.presenceOfElementLocated (By.xpath("//button[text()=\"Fonts\"]"))).click();
-        new WebDriverWait(driver,15).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class=\"mw-ui-checkbox\"]/input"))).click();
-
-        driver.findElement(By.xpath("//button[text()=\"Apply settings\"]")).click();
+        new WebDriverWait(driver,15).until(ExpectedConditions.presenceOfElementLocated(
+                By.xpath("//button[text()=\"Apply settings\"]"))).click();
 
 
 
